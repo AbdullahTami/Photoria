07cab09e7c3e7343421e91b7bf5fa271acf1d519
abdullahqayid <3boo0odq0v@gmail.com>
2023-11-30 02:21:13 +0300

    initial commit

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+// prettier.config.js
+
+// prettier.config.js
+
+export default {
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+// Try doing an export default to the plugin in your prettier.config file and change the file extension to mjs instead of js or cjs as mentioned from jonas
